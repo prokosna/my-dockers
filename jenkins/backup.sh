510ced19f5302903617d48928de62d9d65b766c8
prokosna <prokosna@gmail.com>
2017-05-17 01:26:10 +0900

    Add jenkins

---
+++ jenkins/backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DATA_CONTAINER=jenkins_data_1
+
+cd ~/docker/jenkins
+
+echo "INFO: Stopping containers..."
+docker-compose stop
+
+echo "INFO: Backup starts..."
+docker run --rm --volumes-from $DATA_CONTAINER -v $(pwd)/backup:/backup boombatower/docker-backup backup "$(date +%F_%R).tar.xz"
+echo "INFO: Done!"
+
+echo "INFO: Restarting containers..."
+docker-compose start
+echo "INFO: Now containers in service"
