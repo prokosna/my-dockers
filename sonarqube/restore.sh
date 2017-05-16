a200d99c65bf4ad55fc2d582a99a4e773157197b
prokosna <prokosna@gmail.com>
2017-05-17 02:24:16 +0900

    Add sonarqube

---
+++ sonarqube/restore.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+  echo "Usage: ./restore.sh [backup_file_name]"
+  exit 1
+fi
+
+BACKUP_FILE=$1
+DATA_CONTAINER=sonarqube_data_1
+
+cd ~/docker/sonarqube
+
+echo "INFO: Stopping containers..."
+docker-compose stop
+
+echo "INFO: Restoring..."
+docker run --rm --volumes-from $DATA_CONTAINER -v $(pwd)/backup:/backup boombatower/docker-backup restore $BACKUP_FILE
+echo "INFO: Done!"
+
+echo "INFO: Restarting containers..."
+docker-compose start
+echo "INFO: Now containers in service"
